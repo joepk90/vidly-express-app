18c4538289edc8b40dda3c585b31e34c7f257a9e
Joe Park-Kennaby <jparkkennaby@gmail.com>
2020-03-30 00:12:49 +0100

    fix environement settings - remove unuseful package.json script comment (kest automatically sets NODE_ENV to test. make the mongodb logic use database relevant to environement)

--- startup/mongodb.js
+++ startup/mongodb.js
@@ -1,12 +1,16 @@
 
 const winston = require('winston');
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = function() {
 
-    mongoose.connect('mongodb://localhost/vidly')
-    .then( () => winston.info('Connected to MongoDB...'))
+    const db = config.get('db');
+
+    mongoose.connect(db)
+    .then( () => winston.info(`Connected to ${db}...`))
     // to connect to mongoDB, make sure mongodb is running on localhost. run:
     // mongod
 
 }
+
