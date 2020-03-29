a0ca8ac8a593ac33ddfdfb6a6dc87292e16c1ce0
Joe Park-Kennaby <jparkkennaby@gmail.com>
2020-03-29 20:48:18 +0100

    13.5 - setup more aaccurate test for should return all genres test-suite. import Genre model and add genrea to test database. setup more accurate check against reponse. Setup Test Genre data to be romoved using afterEach jest hook

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,5 +1,6 @@
 // import { describe } from "joi";
 const request = require('supertest');
+const { Genre } = require('../../models/genre.js');
 let server;
 
 describe('/api/genres/', () => {
@@ -8,18 +9,27 @@ describe('/api/genres/', () => {
         server = require('../../index');
     });
 
-    afterEach(() => {
+    afterEach( async () => {
         server.close();
+        await Genre.remove({});
     });
 
     describe('GET /', () => {
 
-        it('should retrun all genres', async () => {
+        it('should return all genres', async () => {
+
+            // add genres to test database 
+            await Genre.collection.insertMany([
+                    {name: 'genre1'},
+                    {name: 'genre2'},
+                    ],
+                );
+
             const res = await request(server).get('/api/genres/');
             expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
+            
         });
 
     });
-
-
 });
