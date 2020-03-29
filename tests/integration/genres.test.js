5161738ca2d6f5ee5ddf771296b639ad44460bb7
Joe Park-Kennaby <jparkkennaby@gmail.com>
2020-03-29 21:35:58 +0100

    13.5 - setup more accurate test for the should return all genres test-suite. check genre response genre name matches added genres

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -28,7 +28,8 @@ describe('/api/genres/', () => {
             const res = await request(server).get('/api/genres/');
             expect(res.status).toBe(200);
             expect(res.body.length).toBe(2);
-            
+            expect(res.body.some(g => g.name === "genre1")).toBeTruthy();
+            expect(res.body.some(g => g.name === "genre2")).toBeTruthy();            
         });
 
     });
