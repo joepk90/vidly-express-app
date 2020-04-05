63acfeb16440d0da0e66bfb6b30ab059c1059fcd
Joe <jparkkennaby@gmail.com>
2020-04-05 23:05:23 +0100

    12.14 - refactor last test. use object keys of the returned body to check for mathcing properties

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -173,11 +173,9 @@ describe('/api/returns', () => {
         const res = await exec();
     
         // const savedRental = await Rental.findById(rental._id);
-        expect(res.body).toHaveProperty('dateOut');
-        expect(res.body).toHaveProperty('dateReturned');
-        expect(res.body).toHaveProperty('rentalFee');
-        expect(res.body).toHaveProperty('customer');
-        expect(res.body).toHaveProperty('movie');
+
+        expect(Object.keys(res.body)).toEqual(
+            expect.arrayContaining(['dateOut', 'dateReturned', 'rentalFee', 'customer', 'movie' ]));
 
     });
 
